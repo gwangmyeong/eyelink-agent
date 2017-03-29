b6c9651f1c540d4a53b44d5577d99e7a581ab9f5
배성한 <furtur3@gmail.com>
2017-03-29 18:24:38 +0900

    developing addTransformer 추가

--- src/main/java/com/m2u/eyelink/agent/ELAgent.java
+++ src/main/java/com/m2u/eyelink/agent/ELAgent.java
@@ -1,9 +1,22 @@
 package com.m2u.eyelink.agent;
 
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
 
 public class ELAgent {
 	public static void premain(String agentArgs, Instrumentation instrumentation) {
 		System.out.println("#### Hello EyeLink Java Agent!!!! ###");
+		instrumentation.addTransformer(new ClassFileTransformer() {
+			public byte[] transform(ClassLoader l, String name, Class c,
+					ProtectionDomain d, byte[] b)
+					throws IllegalClassFormatException {
+				byte[] ret = null; 
+				
+				/* TODO do somthing and fill ret */
+				return ret;
+			}
+		});
 	}
 }
