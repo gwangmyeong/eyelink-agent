c99b5c7dae0d9757b07da350e6371b4e3414d0fc
배성한 <furtur3@gmail.com>
2017-06-08 19:57:35 +0900

    fix, urlclassload not work

--- bootstrap/src/test/java/com/m2u/eyelink/agent/ELAgentURLClassLoaderTest.java
+++ bootstrap/src/test/java/com/m2u/eyelink/agent/ELAgentURLClassLoaderTest.java
@@ -1,6 +1,8 @@
 package com.m2u.eyelink.agent;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -43,22 +45,24 @@ public class ELAgentURLClassLoaderTest {
 		File file = new File(
 				"/Users/baesunghan/git/m2u/pinpoint/quickstart/agent/target/pinpoint-agent/pinpoint-bootstrap-1.6.1-SNAPSHOT.jar");
 		File file2 = new File(
-				"/Users/baesunghan/git/m2u/eyelink-agent/bootstrap/target/lib/netty-3.10.6.Final1.jar");
+				"/Users/baesunghan/git/m2u/eyelink-agent/bootstrap/target/lib/netty-3.10.6.Final.jar");
 
 //		URL url = file.toURL();
 		URL[] urls = new URL[] { file.toURL(), file2.toURL() };
 
 		ClassLoader cl = new URLClassLoader(urls);
 //		Class cls = cl.loadClass("org.jboss.netty.channel.group.ChannelGroup");
+//		Class cls3 = cl.loadClass("org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory");
 //		Class cls2 = cl.loadClass("com.navercorp.pinpoint.bootstrap.LoadState");
 	}
 
 	public static void main(String[] args) {
 		ELAgentURLClassLoaderTest et = new ELAgentURLClassLoaderTest();
 		ChannelClassLoaderTest ccl = new ChannelClassLoaderTest();
-		ccl.load();
 		try {
 			et.testJarFile();
+			loadJarIndDir("/Users/baesunghan/git/m2u/eyelink-agent/bootstrap/target/lib/");
+			ccl.load();
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -68,4 +72,33 @@ public class ELAgentURLClassLoaderTest {
 		}
 	}
 
+    public static void loadJarIndDir(String dir){
+        try{
+            final URLClassLoader loader = (URLClassLoader)ClassLoader.getSystemClassLoader();
+            final Method method = URLClassLoader.class
+                                       .getDeclaredMethod("addURL", new Class[]{URL.class});
+            method.setAccessible(true);
+             
+            new File(dir).listFiles(new FileFilter() {
+                public boolean accept(File jar) {
+                    // jar 파일인 경우만 로딩
+                    if( jar.toString().toLowerCase().contains(".jar") ){
+                        try{
+                            // URLClassLoader.addURL(URL url) 메소드 호출
+                            method.invoke(loader, new Object[]{jar.toURI().toURL()});
+                            System.out.println(jar.getName()+ " is loaded.");
+                        }catch(Exception e){
+                            System.out.println(jar.getName()+ " can't load.");
+                        }
+                    }
+                    return false;
+                }
+            });
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
+	
 }
